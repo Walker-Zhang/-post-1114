f60ec5f7b5ef0055deaa83011bf6b2688ae3a375
yfzhang <yfzhang@yfzhangdeMac-mini.local>
2016-11-14 16:44:27 +0800

    Initial Commit

---
+++ 原生post请求1114/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  原生post请求1114
+//
+//  Created by yfzhang on 2016/11/14.
+//  Copyright © 2016年 yfzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 原生post请求1114/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  原生post请求1114
+//
+//  Created by yfzhang on 2016/11/14.
+//  Copyright © 2016年 yfzhang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
